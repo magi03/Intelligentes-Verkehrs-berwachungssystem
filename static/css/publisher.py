c191b5c84ce4b5f5699ddbc811db4f191e2d94cd
Магдалина Николаева <201222032@fdiba.tu-sofia.bg>
2026-06-23 22:10:30 +0300

    fixes

--- static/css/publisher.py
+++ static/css/publisher.py
@@ -3,7 +3,7 @@ import json
 import time
 import random
 
-BROKER = "172.20.10.5" # Ändere dies zu 127.0.0.1, wenn du ohne Jetson testest
+BROKER = "172.20.10.5" 
 TOPIC = "traffic/intersection1/data"
 
 client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION1, "Jetson_Nano_Simulator")
@@ -12,15 +12,14 @@ client.connect(BROKER)
 print(" Starte AI Traffic Simulation für alle 4 Spuren...")
 
 while True:
-    # Jetzt simulieren wir Spur 1, 2, 3 UND 4
     for lane in [1, 2, 3, 4]:
         payload = {
             "lane_id": lane,
             "car_count": random.randint(0, 15),
             "light_status": random.choice(["Grün", "Gelb", "Rot"]),
             "emergency": "Ja" if random.random() < 0.05 else "Nein",
-            "crash": "Nein",     # <-- NEU hinzugefügt
-            "violation": "Nein"  # <-- NEU hinzugefügt
+            "crash": "Nein",     
+            "violation": "Nein" 
         }
         client.publish(TOPIC, json.dumps(payload))
         print(f"Sent data for Lane {lane}: {payload}")
